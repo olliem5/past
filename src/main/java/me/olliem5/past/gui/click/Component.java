8ff9583ccf6608db568d50ad000ea77d26ac705e
LittleDraily <itzpatrickd@gmail.com>
2020-09-29 20:34:01 +0200

    Setting slider in the whole frame closes then opens new one to prefent setting bars to collide

--- src/main/java/me/olliem5/past/gui/click/Component.java
+++ src/main/java/me/olliem5/past/gui/click/Component.java
@@ -9,4 +9,5 @@ public class Component {
     public void mouseClicked(int mouseX, int mouseY, int button) {}
     public void mouseReleased(int mouseX, int mouseY, int mouseButton) {}
     public void keyTyped(char typedChar, int key) {}
+    public void closeallsub(){}
 }
