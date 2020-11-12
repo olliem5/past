984dce3cd8954f0351b5c63d777c01054ffb8a8c
ollie <olliemullen33@gmail.com>
2020-11-12 20:14:22 +1100

    Rainbow mode in SkyColour

--- src/main/java/me/olliem5/past/module/modules/render/SkyColour.java
+++ src/main/java/me/olliem5/past/module/modules/render/SkyColour.java
@@ -7,6 +7,8 @@ import me.olliem5.past.settings.Setting;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.awt.*;
+
 public class SkyColour extends Module {
     public SkyColour() {
         super ("SkyColour", "Changes the colours of the sky, best in nether & low render distance", Category.RENDER);
@@ -15,6 +17,7 @@ public class SkyColour extends Module {
     Setting red;
     Setting green;
     Setting blue;
+    Setting rainbow;
     Setting density;
 
     @Override
@@ -22,14 +25,26 @@ public class SkyColour extends Module {
         Past.settingsManager.registerSetting(red = new Setting("Red", "SkyColourRed", 0, 255, 255, this));
         Past.settingsManager.registerSetting(green = new Setting("Green", "SkyColourGreen", 0, 10, 255, this));
         Past.settingsManager.registerSetting(blue = new Setting("Blue", "SkyColourBlue", 0, 10, 255, this));
+        Past.settingsManager.registerSetting(rainbow = new Setting("Rainbow", "SkyColourRainbow", false, this));
         Past.settingsManager.registerSetting(density = new Setting("Density", "SkyColourDensity", 0, 0, 255, this));
     }
 
     @SubscribeEvent
     public void fogColours(EntityViewRenderEvent.FogColors event) {
-        event.setRed(red.getValueInt() / 255f);
-        event.setGreen(green.getValueInt() / 255f);
-        event.setBlue(blue.getValueInt() / 255f);
+        float[] hue = new float[]{(float) (System.currentTimeMillis() % 7500L) / 7500f};
+        int rgb = Color.HSBtoRGB(hue[0], 0.8f, 0.8f);
+        int r = rgb >> 16 & 255;
+        int g = rgb >> 8 & 255;
+        int b = rgb & 255;
+        if (rainbow.getValBoolean()) {
+            event.setRed(r / 255f);
+            event.setGreen(g / 255f);
+            event.setBlue(b / 255f);
+        } else {
+            event.setRed(red.getValueInt() / 255f);
+            event.setGreen(green.getValueInt() / 255f);
+            event.setBlue(blue.getValueInt() / 255f);
+        }
     }
 
     @SubscribeEvent
